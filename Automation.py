a94b14a48bd516df828b0b23346fb2f50d8c4adf
xmortalhazardx <cask.overpower_13@hotmail.com>
2025-07-23 12:21:25 -0300

    Atualizando as funcionalidades da automação 4

--- Automation.py
+++ Automation.py
@@ -34,25 +34,55 @@ while True:
     # buton = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//input[@onclick='buscaRapida();']")))
     # buton.click()
 
-    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID,"opcaoBoxNovoChamado")))    
+    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+        (By.ID,"opcaoBoxNovoChamado")))    
     buton.click() 
 
-    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID,"abrirSelecaoSolicitante")))    
+    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+        (By.ID,"abrirSelecaoSolicitante")))    
     buton.click()    
 
-    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, "//a[@class='k-link' and normalize-space(text())='Login']/preceding-sibling::a[@class='k-grid-filter']")))    
-    buton.click()
+    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+        (By.XPATH, "//a[@class='k-link' and normalize-space(text())='Login']/preceding-sibling::a[@class='k-grid-filter']")))    
+    buton.click()    
 
-    buton = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//input[@data-bind='value:filters[0].value']")))
+    buton = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH,"//input[@data-bind='value:filters[0].value']")))
     buton.click()
-    buton.send_keys("cesar.emc")
+    buton.send_keys("cesar.emc") 
+
+    time.sleep(5)   
     
-    buton = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//button [@type='submit']")))
+    buton = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH,"//button [@type='submit']")))
+    buton.click()
+    
+
+    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+        (By.XPATH, "//span[contains(@class, 'k-icon') and contains(@class, 'k-i-tick')]")))     
+    buton.click()    
+
+    buton = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((
+    By.XPATH, "//span[contains(@class, 'k-in') and contains(normalize-space(.), 'TI')]")))
     buton.click()
 
-
-    buton = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, "//span[contains(@class, 'k-icon') and contains(@class, 'k-i-tick')]")))    
+    buton = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((
+    By.XPATH, "//span[contains(@class, 'k-in') and contains(normalize-space(.), 'Pastas de Rede')]")))
     buton.click()
+    
+    buton = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((
+    By.XPATH, "//span[contains(@title, 'Pastas de Rede - Dúvidas') and contains(normalize-space(.), 'Dúvidas')]")))
+    buton.click()
+
+    buton = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//textarea[@id='Motivo']")))
+    buton.send_keys("""TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE TESTE""")
+
+    buton = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((
+    By.XPATH, "//span[contains(@id, 'Salvar') and contains(normalize-space(.), 'Salvar')]")))
+    buton.click()
+
+    
+       
 
     
     time.sleep(100)    
