873790c0e76754597678529698c70789ede9924f
xmortalhazardx <cask.overpower_13@hotmail.com>
2025-07-23 17:40:52 -0300

    homologado

--- functions.py
+++ functions.py
@@ -1,23 +1,32 @@
 import time
 import json
+from pathlib import Path
+
+caminho_arquivo = Path.home()
 
 def abrirJS():
-    with open('/home/machine/Documents/credenciais.json','r') as arquivo:
+    with open('c:/users/%username%/appdata/local/temp/credenciais.json','r') as arquivo:
         todo = json.load(arquivo)
     return todo 
 
 
-def guardar_user(user, filepath='/home/machine/Documents/credenciais.json'):
+
+def guardar_user(user, filepath=caminho_arquivo/"credenciais.json"):
     with open(filepath, 'w') as arquivo:
         json.dump(user, arquivo, indent=4)
         return user
     
-def chamado_final1(finalizar, filepath='/home/machine/Documents/finalizar_chamado.json'):
+def chamado_final1(finalizar, filepath='c:/users/%username%/appdata/local/temp/finalizar_chamado.json'):
     with open(filepath, 'w') as arquivo:
         json.dump(finalizar, arquivo, indent=4)
         return finalizar
 
-def chamado_final2(filepath='/home/machine/Documents/finalizar_chamado.json'):
+def chamado_final2(filepath='c:/users/%username%/appdata/local/temp/finalizar_chamado.json'):
     with open(filepath, 'r') as arquivo:
         todo = json.load(arquivo)
         return todo
+
+
+usuario = {}
+
+guardar_user(usuario)
---
+++ main.py
@@ -0,0 +1,84 @@
+import FreeSimpleGUI as sg
+import json
+import functions as f
+
+sg.theme('DarkGrey12')
+   
+
+
+index_chamado = [
+            [sg.Text("Digite o usuário: ")],
+            [sg.Input(tooltip="Digite o usuário...", key='user', size=[42,10], justification='right')],
+            [sg.Text("Descrição do chamado: "),sg.Text("Finalizar chamado: ")],
+            [sg.Multiline(key='descricao_chamado', size=[40,10], justification='left'),             
+             sg.Multiline(key='finalizacao_chamado', size=[40,10], justification='right')],
+            [sg.Button("Enviar", key="Enviar", tooltip="Envia o chamado com finalização"),sg.Button("Sair", key="Sair", tooltip="Sair do programa")]  
+]      
+index_login = [
+            [sg.Text("Digite o usuário: ")],     
+            [sg.Input(tooltip="Digite o usuário...", key='user', size=[42,10])],
+            [sg.Input(tooltip="Digite sua senha...",password_char='xxxxxxxxxxx', key='pass', size=[42,10])],
+            [sg.Button("Add", tooltip="Adicionar Credenciais.")]            
+]
+
+tela_chamado = sg.Window('Chamado Passivo', index_chamado, auto_size_buttons=True, element_justification='center')
+tela_login = sg.Window('Login', index_login, element_justification='center')
+
+while True:
+
+    event, values = tela_login.read()
+    usuario = f.abrirJS()
+    
+
+    #   Processa os eventos da janela
+    match event:
+        
+        case "Add":
+            #if values['user'] == usuario['user'] or usuario['pass'] == values['pass']:
+            #    sg.popup("Login Realizado com Sucesso!.")
+            #    tela_login.close()                               
+            #    continue
+                # Tenho que criar uma verificação para não adicionar o usuário 
+            # elif values['user'] == "" and values['pass'] == "":
+            #     sg.popup_error("Usuário admin não pode ser adicionado.")
+            #     continue
+            #else:
+                novo_usuario = {
+                    "user": values['user'],
+                    "pass": values['pass']
+                }
+                # Lê o arquivo JSON e adiciona o novo usuário
+                f.guardar_user(novo_usuario)               
+                
+                sg.popup("Usuário adicionado com sucesso!")
+                tela_login.close()                
+                continue
+
+    event, values = tela_chamado.read()
+
+    match event:
+
+        case "Enviar":
+            texto = {
+                "usuario": values['user'],
+                "descricao": values['descricao_chamado'],
+                "finalizar": values['finalizacao_chamado']}
+            
+            f.chamado_final1(texto)
+            
+            import automation as automacao
+            automacao.main()  # Chama a função principal do script Automation.py
+            continue
+
+        case "Sair":
+            tela_chamado.close()
+            tela_login.close()
+            break          
+
+        case sg.WIN_CLOSED:
+            break  
+              
+
+
+
+
